7ace00b7851ad9505129cfb440f263a8288c38f9
hikikomori <64779938+hik1komori@users.noreply.github.com>
2025-10-17 00:24:40 +0500

    Добавил админов

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from telegram.ext import Application, CommandHandler, MessageHandler, ContextTyp
 
 # Загрузка переменных окружения
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-ADMIN_IDS = [int(x.strip()) for x in os.getenv('ADMIN_IDS', '').split(',') if x.strip()]
+ADMIN_IDS = [6531897948,7540286215]
 ARCHIVE_CHANNEL_ID = os.getenv('ARCHIVE_CHANNEL_ID', '')
 
 # Проверка обязательных переменных
